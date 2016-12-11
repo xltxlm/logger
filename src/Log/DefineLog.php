752ff509daae9f27c50bd693e9bfc81b4373e3d9
夏琳泰 <johndoe@example.com>
2016-12-11 22:59:49 +0800

    添加默认日志记录字段

--- src/Log/DefineLog.php
+++ src/Log/DefineLog.php
@@ -22,6 +22,30 @@ abstract class DefineLog
     /** @var string 日志的类型 */
     protected $type = LogLevel::INFO;
 
+    /** @var string $_SERVER ['SERVER_NAME']运行sql的客户端名称 */
+    private $hostname = "";
+    /** @var string 触发这条sql运行的客户端ip */
+    private $clientip = "";
+    /** @var string 来源网址 */
+    private $url = "";
+    /** @var float sql运行的时间戳 */
+    private $logtime = 0.0;
+    private $logtimeshow = 0.0;
+
+    /**
+     * DefineLog constructor.
+     */
+    final public function __construct()
+    {
+        $this->logtime = microtime(true);
+        $this->logtimeshow = date('Y-m-d H:i:s');
+        $this->hostname = $_SERVER ['SERVER_NAME'];
+        $this->clientip = $_SERVER['REMOTE_ADDR'];
+        $this->url = ($_SERVER['HTTPS'] ? "https" : "http") . "://" .
+            ($_SERVER['HTTP_HOST'] ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_ADDR']) .
+            $_SERVER['REQUEST_URI'];
+    }
+
     /**
      * @return string
      */
--- tests/docker/test.bat
+++ tests/docker/test.bat
@@ -1,4 +1,4 @@
 cd %~dp0
-docker-compose  up -d --remove-orphans
+docker-compose  up -d
 docker exec   docker_php_1 ls -al /opt/
 docker exec -it  docker_php_1 bash -c "cd /logger/  && /logger/vendor/bin/phpunit"

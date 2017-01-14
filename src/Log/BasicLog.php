7c8257ec87523faf86c7caf8cf6b5f69ba74568a
夏琳泰 <xltxlm@qq.com>
2017-01-14 23:19:38 +0800

    BasicLog 改进为通用的打点日志类

--- src/Log/BasicLog.php
+++ src/Log/BasicLog.php
@@ -3,21 +3,22 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2016-12-18
- * Time: 下午 12:06
+ * Time: 下午 12:06.
  */
 
 namespace xltxlm\logger\Log;
 
+use xltxlm\helper\Hclass\ConvertObject;
+
 /**
  * 最基础的日志统计:记录一个字符串
- * Class BasicLog
- * @package xltxlm\logger\Log
+ * Class BasicLog.
  */
 class BasicLog extends DefineLog
 {
-    protected $message = "";
+    protected $message = '';
 
-    protected function __selfConstruct($message = "")
+    protected function __selfConstruct($message = '')
     {
         if ($message) {
             $this->setMessage($message);
@@ -33,12 +34,19 @@ class BasicLog extends DefineLog
     }
 
     /**
-     * @param string $message
+     * @param mixed $message
+     *
      * @return BasicLog
      */
-    public function setMessage(string $message): BasicLog
+    public function setMessage($message): BasicLog
     {
+        if (is_object($message)) {
+            $message = (new ConvertObject($message))
+                ->toJson();
+        }
+        $this->setLogClassName(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1]['class']);
         $this->message = $message;
+
         return $this;
     }
 }
